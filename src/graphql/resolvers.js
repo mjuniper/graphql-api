2ba99100b72b0394a7d6de7c6f7990429f919307
mjuniper <mjuniper@gmail.com>
2019-11-27 13:15:34 -0700

    get surveys working; setup elastic beanstalk

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,25 +1,28 @@
 const fetch = require('node-fetch')
 
-const token = '6vhcoqwWPfFBkUvgxeRi1k0lkRNnXYrEizeKnrdqBrKIf8Ifvzq54im1DeHItzpmKTuun5PIGrSqJlH_AMC4LgxSz7h9bJC0TqosJ1ghFfmdTRu60ZIzm1BhrGrFMkDHqv2y3zHflo8mncw1KIX6Qeq8eOSNg70lb-0PH5QZTQmu7csoQtbZtvefEXhoQaY_D0_19FfW6cNCJl9dHE4viA..';
 const agoBaseUrl = 'https://dc.mapsqa.arcgis.com/sharing/rest';
 
 const log = url => {
   console.log(`>> ${url}`);
   return url;
 };
-const get = url => {
+const get = (url, ctx) => {
   log(url);
+  const token = ctx.request.headers.authorization;
+  if (token) {
+    url = `${url}&token=${token}`;
+  }
   return fetch(url)
   .then(res => res.json());
 };
 
 exports.resolvers = {
   Query: {
-    surveys: (parent, args = {}) => {
+    surveys: (parent, args = {}, ctx) => {
       // TODO: q, num, start, sortField, sortOrder
       // TODO: really this is just a special kind of items search so we might delegate to an items query...
+      console.log('Got request for surveys');
       const { groups, type, q } = args;
-      // console.log(groups);
 
       const qParts = [
         'type:Form',
@@ -40,14 +43,25 @@ exports.resolvers = {
 
       const query = qParts.join(' AND ');
 
-      const url = `${agoBaseUrl}/search?f=json&q=${query}&token=${token}`;
-      return get(url)
+      const url = `${agoBaseUrl}/search?f=json&q=${query}`;
+      return get(url, ctx)
       .then(resp => resp.results);
     },
-    survey: (parent, args) => {
+    survey: (parent, args, ctx) => {
+      console.log('Got request for survey');
       const { id } = args;
-      const url = `${agoBaseUrl}/content/items/${id}?f=json&token=${token}`;
-      return get(url);
-    },
-  }
+      const url = `${agoBaseUrl}/content/items/${id}?f=json`;
+      return get(url, ctx);
+    }
+  },
+    Survey: {
+      formInfo: (parent, args, ctx) => {
+        const { id } = parent;
+        const url = `${agoBaseUrl}/content/items/${id}/info/form.json?f=json`;
+        return get(url, ctx)
+        .then(resp => {
+          return resp.settings.openStatusInfo;
+        });
+      }
+    }
 }
--- src/index.js
+++ src/index.js
@@ -2,34 +2,79 @@ const { GraphQLServer } = require('graphql-yoga')
 const resolvers = require('./graphql/resolvers').resolvers;
 
 /*
+  TODO:
+    - deploy to surge
+    - push upstream
+    - use arcgis-rest-js
+    - implement count info
+*/
+
+/*
   Issues to overcome when using this with ago api:
     - search, sort, pagination...
     - num has max of 100
-    - token
+    - token/authorization
     - no node in enterprise!!!
     - [use arcgis-rest-js]
+    - avoid simply mirroring the ago api - this is an opportunity to make it better!
 */
 
 const server = new GraphQLServer({
   typeDefs: `./src/graphql/schema.graphql`,
   resolvers,
+  context: args => {
+    return { ...args, jupe: 'jupe' };
+  }
 })
 
 const opts = {
   defaultPlaygroundQuery: `
-query surveys {
-  surveys(type:"published" groups:["2f4d28abfdea4ffab5ec4c5c54574ef8", "95c6c8202a0e4f189ca0aad12a10d501"]) {
-    id
-    title
-    description
-    typeKeywords
+# to make authenticated requests, paste this into the http headers below:
+# {"authorization":"your-token"}
+
+fragment surveyFields on Survey {
+  id
+  title
+  description
+}
+
+fragment surveyFieldsWithSchedule on Survey {
+  id
+  title
+  formInfo {
+    status
+    schedule {
+      start
+      end
+    }
   }
 }
 
+# this will make a 1 request to ago (the search for the survey items)
+query surveys {
+  surveys(type:"published" groups:["2f4d28abfdea4ffab5ec4c5c54574ef8", "95c6c8202a0e4f189ca0aad12a10d501"]) {
+    ...surveyFields
+  }
+}
+
+# this will make a n + 1 requests to ago (one for the search and then one for the formInfo for each result)
+query surveysWithSchedules {
+  surveys(type:"published" groups:["2f4d28abfdea4ffab5ec4c5c54574ef8", "95c6c8202a0e4f189ca0aad12a10d501"]) {
+    ...surveyFieldsWithSchedule
+  }
+}
+
+# this will make 1 request to ago (for the specified survey item)
 query survey {
-  survey(id:"3ce0ef773c3445e29bc881503f366047") {
-    id
-    title
+  survey(id:"680f7cef6b634dfcaec6b68b5008edf6") {
+    ...surveyFields
+  }
+}
+
+# this will make 2 requests to ago (one for the specified item and one for its formInfo)
+query surveyWithSchedule {
+  survey(id:"680f7cef6b634dfcaec6b68b5008edf6") {
+    ...surveyFieldsWithSchedule
   }
 }
 `,
